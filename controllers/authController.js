2e64d7e3b0279be61fe64b1d240134a025e70c3a
eslamsemedo <eslamsemedo@gmail.com>
2025-10-13 18:08:41 +0300

    Remove restriction preventing super admins from deleting their own accounts in deleteUser function

--- controllers/authController.js
+++ controllers/authController.js
@@ -224,11 +224,12 @@ export async function updateUser(req, res, next) {
 export async function deleteUser(req, res, next) {
   try {
     const { id } = req.body || {};
+    // console.log(req.body)
     if (!id) return next(new AppError("User ID is required", 400));
 
-    if (req.admin.role === "SUPER_ADMIN") {
-      return next(new AppError("You cannot delete your own super account", 400));
-    }
+    // if (req.admin.role === "SUPER_ADMIN") {
+    //   return next(new AppError("You cannot delete your own super account", 400));
+    // }
 
     const deletedUser = await AdminUser.findByIdAndDelete(id).select('-password');
     if (!deletedUser) return next(new AppError("User not found", 404));
