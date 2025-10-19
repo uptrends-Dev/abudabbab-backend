d5c16bacecb47fa2ae2d2a3e2be119ee93fd0e84
eslamsemedo <eslamsemedo@gmail.com>
2025-10-19 16:02:20 +0300

    auth on coupon

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { createCoupon, deleteCoupon, getAllCoupons, getCoupon, updateCoupon } from "../controllers/couponController.js";
+import { allowedTo, requireAdmin } from "../midelWares/authHandler.js";
 
 
 
 const couponRouter = Router();
 
 // Define coupon-related routes here
-couponRouter.post("/create", createCoupon);
-couponRouter.get("/getall", getAllCoupons);
-couponRouter.get("/get/:id", getCoupon);
-couponRouter.put("/update/:id", updateCoupon);
-couponRouter.delete("/delete/:id", deleteCoupon);
+couponRouter.post("/create", requireAdmin, allowedTo("admin"), createCoupon);
+couponRouter.get("/getall", requireAdmin, allowedTo("admin"), getAllCoupons);
+couponRouter.get("/get/:id", requireAdmin, allowedTo("admin"), getCoupon);
+couponRouter.put("/update/:id", requireAdmin, allowedTo("admin"), updateCoupon);
+couponRouter.delete("/delete/:id", requireAdmin, allowedTo("admin"), deleteCoupon);
 
 export default couponRouter;
