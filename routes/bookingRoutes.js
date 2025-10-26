20043fe996700801fae1427e0d1c447eebbf4409
reyad <reyadmohamed631@gmail.com>
2025-10-26 13:35:33 +0300

    ..

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -14,14 +14,11 @@ import { allowedTo, requireAdmin } from "../midelWares/authHandler.js";
 const router = express.Router();
 
 router.get("/admin", requireAdmin, allowedTo("EMPLOYEE", "ADMIN", "FINANCE"), getAllBookings);
-router.get("/admin/:id", requireAdmin, allowedTo("EMPLOYEE", "ADMIN", "FINANCE"), getBookingById);
+router.get("/admin/:id", requireAdmin, allowedTo("EMPLOYEE", "ADMIN", "FINANCE" , "GATE"), getBookingById);
 router.post("/export", requireAdmin, allowedTo("ADMIN", "FINANCE"), exportBookings);
-
 router.patch("/admin/:id", requireAdmin, allowedTo("ADMIN", "GATE"), updateBookingState);
-
 router.get("/advancedTripsInfos/admin", requireAdmin, allowedTo("FINANCE"), advancedTripsInfos);
 router.get("/getTotalBookingsAndRevenue/admin", requireAdmin, allowedTo("FINANCE"), getTotalBookingsAndRevenue);
-
 router.post("/", createBooking);
 
 export default router;
