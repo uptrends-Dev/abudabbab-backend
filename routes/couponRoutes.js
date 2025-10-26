5c36c95d8915075a5dd4493efbcf8e51d0a4ef29
reyad <reyadmohamed631@gmail.com>
2025-10-26 13:08:13 +0300

    fix

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -7,12 +7,12 @@ import { allowedTo, requireAdmin } from "../midelWares/authHandler.js";
 const couponRouter = Router();
 
 // Define coupon-related routes here
-couponRouter.post("/", requireAdmin, allowedTo("admin"), createCoupon);
-couponRouter.get("/", requireAdmin, allowedTo("admin"), getAllCoupons);
-couponRouter.get("/:id", requireAdmin, allowedTo("admin"), getCoupon);
-couponRouter.put("/:id", requireAdmin, allowedTo("admin"), updateCoupon);
-couponRouter.delete("/:id", requireAdmin, allowedTo("admin"), deleteCoupon);
-couponRouter.patch("/:id/toggle", requireAdmin, allowedTo("admin"), toggleCoupon);
+couponRouter.post("/", requireAdmin, allowedTo("ADMIN"), createCoupon);
+couponRouter.get("/", requireAdmin, allowedTo("ADMIN"), getAllCoupons);
+couponRouter.get("/:id", requireAdmin, allowedTo("ADMIN"), getCoupon);
+couponRouter.put("/:id", requireAdmin, allowedTo("ADMIN"), updateCoupon);
+couponRouter.delete("/:id", requireAdmin, allowedTo("ADMIN"), deleteCoupon);
+couponRouter.patch("/:id/toggle", requireAdmin, allowedTo("ADMIN"), toggleCoupon);
 couponRouter.get("/validate/:code", validateCoupon);
 
 export default couponRouter;
